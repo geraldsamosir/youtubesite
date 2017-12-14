eb81aa05879c210e042dc0b8fbf3542db0624415
Gerald Halomoan Samosir <geralddeveloper95@gmail.com>
2017-12-14 13:58:39 +0700

    change the port config

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const next = require('next')
 const routes = require('./router')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
+const port = process.env.PORT || 3000
+
 const handler = routes.getRequestHandler(app)
 
 
 // Without express
 const {createServer} = require('http')
 app.prepare().then(() => {
-    createServer(handler).listen(5000)
+    createServer(handler).listen(port)
 })
